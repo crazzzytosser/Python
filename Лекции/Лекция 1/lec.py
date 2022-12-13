04effb6baff139fcdd25eb7b238137709dd81c13
Your Name <you@example.com>
2022-12-13 22:31:37 +0300

    Lections

---
+++ Лекции/Лекция 1/lec.py
@@ -0,0 +1,10 @@
+# print('Input nuber A: ')
+# A = int(input())
+# print('Input number B: ')
+# B = int(input())
+# print(A,'+', B, '=', A*B)
+# C = A+B
+# print(C)
+
+for i in [2,24,44,32,1]:
+    print(i**3)
