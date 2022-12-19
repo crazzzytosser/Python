730af816a2f6efbd03e389aff56a2634d3f9ebfb
Your Name <you@example.com>
2022-12-19 20:24:34 +0300

    1.2

--- HomeWork/HomeWork1/1.2.py
+++ HomeWork/HomeWork1/1.2.py
@@ -1,5 +1,10 @@
-zadacha 2
+# 2 Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-
-
-12345
+for x in [False, True]:
+    for y in [False, True]:
+        for z in [False, True]:
+            if (not x or y or z) != (not x or y or z):
+                print('Выражение: ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - False')
+                break
+else:
+    print('Выражение: ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - True')
