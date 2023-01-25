51fb2d93a284b8dff8895713f938d49d804d83fa
Your Name <you@example.com>
2023-01-25 21:22:07 +0300

    6.1

--- HomeWork/HomeWork6/6.1.py
+++ HomeWork/HomeWork6/6.1.py
@@ -1,15 +1,6 @@
 # Представлен список чисел. Необходимо вывести элементы исходного списка,
 # значения которых больше предыдущего элемента. Use comprehension.
 
-
-
-# list = [int(s) for s in input().split() \
-#         for i in range(1, len(list)) if list[i] > list[i-1]]
-# print(list)
-
-
-# res = [int(s) for s in input().split() for i in range(1, len(s)) if s[i] > s[i-1]]
-# print(res)
 list = [int(i) for i in input().split()]
 res = [list[i] for i in range(1, len(list)) if list[i] > list[i-1]]
 print(res)
